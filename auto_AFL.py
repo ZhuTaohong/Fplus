6d2f227ace9ebe7d2eb63ea6819663e452ce38fa
ZhuTaohong <116883760+ZhuTaohong@users.noreply.github.com>
2023-10-03 13:01:47 +0100

    Update auto_AFL.py

--- auto_AFL.py
+++ auto_AFL.py
@@ -6,24 +6,7 @@ import queue
 import signal
 import psutil
 
-# commands = ["mkdir build", "CC=/AFLplusplus/afl-clang-fast CXX=/AFLplusplus/afl-clang-fast++ cmake -B ./build",
-#             "make -C ./build"]
-#
-# executable_path = os.getcwd() + "/build/AirplaneApp"
-
 seeds_path = os.path.join(os.getcwd(), "seeds_kth_hanoi")
-
-# crashes_folder = os.path.join(os.getcwd(), "out", "default", "crashes")
-
-
-# def add_AFL_compiler(commands):
-#     for command in commands:
-#         try:
-#             subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
-#
-#         except subprocess.CalledProcessError as e:
-#             print(f"error：\n{e.stderr}")
-
 
 def run_aflplusplus(code_path):
     afl_cmd = ["py-afl-fuzz", "-i", seeds_path, "-o", "out", "--", "python", code_path]
@@ -65,21 +48,5 @@ def get_crash(code_path):
         if not check_afl_fuzz_process():
             break
 
-        # if os.path.exists(crashes_folder):
-        #     files_in_folder = os.listdir(crashes_folder)
-        #
-        #     found = False
-        #     for file_name in files_in_folder:
-        #         if file_name.startswith("id"):
-        #             found = True
-        #             break
-        #
-        # if found:
-        #     try:
-        #         subprocess.run(['killall', 'afl-fuzz'])
-        #         print("afl++ is paused")
-        #
-        #     except subprocess.CalledProcessError:
-        #         print("can not pause afl++")
 
 
